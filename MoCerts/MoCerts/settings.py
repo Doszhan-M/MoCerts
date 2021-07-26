8f8a9b2e0443df3cedf42f6b13f2215b10419b91
Doszhan <Doszhan.m@mail.ru>
2021-07-26 15:54:55 +0600

    add prod settings

--- MoCerts/MoCerts/settings.py
+++ MoCerts/MoCerts/settings.py
@@ -12,18 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+try:
+    from .local_settings import *
+except ImportError:
+    from .prod_settings import *
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$$shb6k7d6&6rw8jjl1i2pe6%c^2_f+ii+)acyt-ols@#g*=v)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -85,18 +79,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'MoCerts.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
