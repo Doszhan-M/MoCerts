0255e485482917c5703c1058253086f727d4962d
Doszhan <Doszhan.m@mail.ru>
2021-07-26 16:27:05 +0600

    add prod settings

--- MoCerts/MoCerts/settings.py
+++ MoCerts/MoCerts/settings.py
@@ -117,7 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
 MEDIA_URL = '/media/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media/')
