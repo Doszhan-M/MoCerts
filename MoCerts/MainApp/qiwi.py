6de7b5a5fb670741480e1ed2538c77cd908aa668
Doszhan <Doszhan.m@mail.ru>
2021-08-01 00:59:11 +0600

    add balance page

--- MoCerts/MainApp/qiwi.py
+++ MoCerts/MainApp/qiwi.py
@@ -1,10 +1,13 @@
 from .models import QiwiSecretKey
 
+try:
+    if QiwiSecretKey.objects.exists():
+        QIWISECRET_KEY = QiwiSecretKey.objects.all()[0].secret_key
+    else:
+        QiwiSecretKey.objects.create()
+        QIWISECRET_KEY = QiwiSecretKey.objects.all()[0].secret_key
+except:
+    QIWISECRET_KEY = 0
 
-if QiwiSecretKey.objects.exists():
-    QIWISECRET_KEY = QiwiSecretKey.objects.all()[0].secret_key
-else:
-    QiwiSecretKey.objects.create()
-    QIWISECRET_KEY = QiwiSecretKey.objects.all()[0].secret_key
 
 
