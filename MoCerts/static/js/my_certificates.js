39b25b723c157b2d4bb66bc79b0c5a9f66f926cc
Doszhan <Doszhan.m@mail.ru>
2021-08-05 14:45:12 +0600

    add dropdown js elem

--- MoCerts/static/js/my_certificates.js
+++ MoCerts/static/js/my_certificates.js
@@ -1,1 +1,3 @@
-console.log(1)
+const dropdown_mobile = document.querySelectorAll('.dropdown_mobile')
+
+dropdown_mobile.forEach(elem => elem.addEventListener("click", function(){ elem.style.zIndex = 99}))
