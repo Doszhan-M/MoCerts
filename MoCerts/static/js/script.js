62a4c202f5bd2e9f448521594a9e71c49d82be35
Doszhan <Doszhan.m@mail.ru>
2021-08-05 14:02:35 +0600

    add adaptiv css

--- MoCerts/static/js/script.js
+++ MoCerts/static/js/script.js
@@ -2,24 +2,24 @@
     
   'use strict'
 
-  var menu = document.querySelector('.menu-wrap'),
-  menuItem = document.querySelectorAll('.menu-link'),
-  hamburger = document.querySelector('.header__hamburger');
+  // var menu = document.querySelector('.menu-wrap'),
+  // menuItem = document.querySelectorAll('.menu-link'),
+  // hamburger = document.querySelector('.header__hamburger');
  
-  hamburger.addEventListener('click', function()  {
-  hamburger.classList.toggle('hamburger_active');
-  menu.classList.toggle('top_active');
+  // hamburger.addEventListener('click', function()  {
+  // hamburger.classList.toggle('hamburger_active');
+  // menu.classList.toggle('top_active');
   
-  });
+  // });
   
-  for (var i=0; i < menuItem.length; i++) {
-    var element = menuItem[i];
-    console.log (element);
-      element.addEventListener('click', function(){
-      hamburger.classList.toggle('hamburger_active');
-      menu.classList.toggle('top_active');
-    });
-  };
+  // for (var i=0; i < menuItem.length; i++) {
+  //   var element = menuItem[i];
+  //   console.log (element);
+  //     element.addEventListener('click', function(){
+  //     hamburger.classList.toggle('hamburger_active');
+  //     menu.classList.toggle('top_active');
+  //   });
+  // };
 
   $('.EN').click(function(){
       $('.select-list').toggle();
